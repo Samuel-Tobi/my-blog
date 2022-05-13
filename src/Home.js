daea9f088db1b72ac04a55833e40923fd5ed4cc5
Samuel-Tobi <oluwatobi9990@gmail.com>
2022-05-13 12:28:52 +0100

    Delete blog button working

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,26 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-  // let name = 'Tobi';
-  const [name, setName] = useState('Tobi');
-  const [age, setAge] = useState(15);
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'Tobi', id: 1 },
+    { title: 'Welcome party', body: 'lorem ipsum...', author: 'Samuel', id: 2 },
+    {
+      title: 'Web dev top tips',
+      body: 'lorem ipsum...',
+      author: 'Tobi',
+      id: 3,
+    },
+  ]);
 
-  const handleClick = () => {
-    setName('Samuel');
-    setAge(20);
+  const handleDelete = id => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
   };
 
   return (
     <div className='home'>
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click me</button>
+      <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />
     </div>
   );
 };
